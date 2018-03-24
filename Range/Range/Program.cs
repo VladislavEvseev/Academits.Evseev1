417afcc98a635d19e893aceae1cb76533b3b86c9
Владислав Евсеев <vladislav.ev@gmail.com>
2018-03-24 17:01:57 +0700

    Старый репозиторий приказал долго жить и умер либо это связано с обновлением семерки до десятки, переделал на новом

---
+++ Range/Range/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Range
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Range r1 = new Range(0, 18);
+
+            double numberInRange = 8;
+            r1.IsInside(numberInRange);
+
+            Range r2 = new Range(1, 19);
+
+            if (r1.GetGeneral(r2) != null)
+            {
+                Range r3;
+                r3 = r1.GetGeneral(r2);
+                Console.WriteLine("Главный интервал: {0} {1}", r3.From, r3.To);
+            }
+            else
+            {
+                Console.WriteLine("Null");
+            }
+
+            Range[] arrayResult;
+
+            arrayResult = r1.GetConcatenationOfIntervals(r2);
+            Console.WriteLine("Добавочные интервалы");
+
+            for (int i = 0; i < arrayResult.Length; i++)
+            {
+                Console.WriteLine("Начало: {0} Конец {1} ", arrayResult[i].From, arrayResult[i].To);
+            }
+
+            arrayResult = r1.GetDifferentIntervals(r2);
+            Console.WriteLine("Разница массивов");
+
+            r1.GetDifferentIntervals(r2);
+            for (int i = 0; i < arrayResult.Length; i++)
+            {
+                Console.WriteLine("Начало: {0} Конец {1} ", arrayResult[i].From, arrayResult[i].To);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Range/Range/Range.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Range
+{
+    class Range
+    {
+        public double From { get; set; }
+        public double To { get; set; }
+
+        public Range(double from, double to)
+        {
+            From = from;
+            To = to;
+        }
+
+        public double Length
+        {
+            get { return To - From; }
+        }
+
+        public bool IsInside(double numberInRange)
+        {
+            return From <= numberInRange && To >= numberInRange;
+        }
+
+        public Range GetGeneral(Range r2)
+        {
+            if ((r2.From >= To) || (From >= r2.To))
+            {
+                return null;
+            }
+            else
+            {
+                if (From >= r2.From && To >= r2.To)
+                {
+                    return new Range(From, r2.To);
+                }
+                else if (To <= r2.To && r2.From >= From)
+                {
+                    return new Range(r2.From, To);
+                }
+                else if (From <= r2.From && To >= r2.To)
+                {
+                    return new Range(r2.From, r2.To);
+                }
+                else
+                {
+                    return new Range(From, To);
+                }
+            }
+        }
+
+        public Range[] GetConcatenationOfIntervals(Range r2)
+        {
+            if ((r2.From > To) || (From > r2.To))
+            {
+                Range[] arrayOfRanges = new Range[2];
+
+                arrayOfRanges[0] = new Range(From, To);
+
+                Range r3 = new Range(r2.From, r2.To);
+                arrayOfRanges[1] = r3;
+
+                return arrayOfRanges;
+            }
+            else
+            {
+                Range[] arrayOfRanges = new Range[1];
+
+                if (From >= r2.From && To >= r2.To)
+                {
+                    arrayOfRanges[0] = new Range(r2.From, To);
+
+                }
+                else if (To <= r2.To && r2.From >= From)
+                {
+                    arrayOfRanges[0] = new Range(From, r2.To);
+                }
+                else if (From <= r2.From && To >= r2.To)
+                {
+                    arrayOfRanges[0] = new Range(From, To);
+                }
+                else if (r2.From >= To)
+                {
+                    arrayOfRanges[0] = new Range(From, r2.To);
+                }
+                else if (From >= r2.To)
+                {
+                    arrayOfRanges[0] = new Range(r2.From, To);
+                }
+                else
+                {
+                    arrayOfRanges[0] = new Range(r2.From, r2.To);
+                }
+                return arrayOfRanges;
+            }
+        }
+
+        public Range[] GetDifferentIntervals(Range r2)
+        {
+            if (From < r2.From && To > r2.To)
+            {
+                Range[] arrayOfRanges = new Range[2];
+
+                arrayOfRanges[0] = new Range(From, r2.From);
+
+                Range r3 = new Range(r2.To, To);
+                arrayOfRanges[1] = r3;
+
+                return arrayOfRanges;
+            }
+            else
+            {
+                Range[] arrayOfRanges = new Range[1];
+
+                if ((r2.From >= To) || (From > r2.To))
+                {
+                    arrayOfRanges[0] = new Range(From, To);
+                    return arrayOfRanges;
+                }
+                else if (From >= r2.From && To > r2.To)
+                {
+                    arrayOfRanges[0] = new Range(r2.To, To);
+                    return arrayOfRanges;
+                }
+                else if (From < r2.From && To <= r2.To)
+                {
+                    arrayOfRanges[0] = new Range(From, r2.From);
+                    return arrayOfRanges;
+                }
+                else
+                {
+                    arrayOfRanges = new Range[0];
+                    return arrayOfRanges;
+                }
+            }
+        }
+    }
+}
